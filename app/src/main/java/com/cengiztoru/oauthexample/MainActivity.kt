97b526dfc6524865934e2b6bdeb63d06b6cf63e7
Cengiz TORU <cengiztoru@gmail.com>
2020-08-19 17:31:28 +0300

    Loading website & Catching data from website

--- app/src/main/java/com/cengiztoru/oauthexample/MainActivity.kt
+++ app/src/main/java/com/cengiztoru/oauthexample/MainActivity.kt
@@ -2,10 +2,17 @@ package com.cengiztoru.oauthexample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        webView.settings.javaScriptEnabled = true
+        webView.addJavascriptInterface(WebAppInterface(this), "CengizTORUMobile")
+        webView.loadUrl("http://192.168.2.180/oauth.html")
+
     }
 }
